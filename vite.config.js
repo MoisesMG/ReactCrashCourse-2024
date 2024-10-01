ae185f021946df0e69a950abf234b3ffd149b7a6
MoisesMG <menagonzalezmoises@gmail.com>
2024-10-01 02:43:21 -0600

    Try to deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/reactcrashcourse-2024/',
   plugins: [react()],
   server: {
-    port: 3000,
+    host: true,
     proxy: {
       '/api':{
         target: 'http://localhost:8000',
